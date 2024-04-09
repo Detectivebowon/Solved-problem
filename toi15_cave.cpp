c03244ab5e6162bbd2e138168958be502c0cc14b
BOWONz <163514212+Detectivebowon@users.noreply.github.com>
2024-04-09 17:46:59 +0700

    Add files via upload

---
+++ toi15_cave.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n,p,u,e;
+vector<pair<int,int>> adj[10007]
+int main()
+{
+    cin>>n>>p>>u>>e;//num start end eg
+    for(int i=1;i<=e;i++)
+    {
+        int u,v,w;
+        cin>>u>>v>>w;
+        adj[u].push_back({w,v});
+    }
+    cin>>l;
+    for(int i=1;i<=l;i++)
+    {
+        cin>>h[i];
+    }
+    //set value
+    for(int i=1;i<=l;i++)
+        for(int j=0;j<n;j++)
+        {
+            dist[i][j]=INT_MAX;
+        }
+    for(int i=1;i<=l;i++)
+    {
+        dist[i][p]=0;
+    }
+    q.emplace()
+    while(!q.empyty())
+    {
+
+    }
+}
